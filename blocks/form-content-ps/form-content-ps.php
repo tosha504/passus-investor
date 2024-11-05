cd6b9141e4a8403e63a554fe11905fffcdb7b6a9
tosha504 <tosha.rovniy@gmail.com>
2024-11-05 11:33:36 +0100

    form-content-ps block create

---
+++ blocks/form-content-ps/form-content-ps.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Form Content PS Block template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during backend preview render.
+ * @param   int $post_id The post ID the block is rendering content against.
+ *          This is either the post ID currently being displayed inside a query loop,
+ *          or the post ID of the post hosting this block.
+ * @param   array $context The context provided to the block by the post or its parent block.
+ */
+
+// Load values and assign defaults.
+
+$anchor = '';
+if (!empty($block['anchor'])) {
+  $anchor = 'id="' . esc_attr($block['anchor']) . '" ';
+}
+$tag = get_field('tag');
+$text_title = get_field('text_title');
+$class_title = get_field('class_title');
+$font_weight = get_field('font-weight');
+$description = !empty(get_field('description')) ? "<div class='form-content-ps__left_description'>" . get_field('description') . "</div>" : "";
+$shortcode = get_field('shortcode');
+$background = !empty(get_field('background')) ? 'style="background-image: url(' . wp_get_attachment_url(get_field('background')) . ')"' : ''; ?>
+<!-- form-content-ps start -->
+<section class="form-content-ps" <?php echo  $anchor . $background; ?>>
+  <div class="container">
+    <div class="form-content-ps__left">
+      <?php show_title_and_btn($tag, $text_title, $class_title, $font_weight);
+      echo $description; ?>
+    </div>
+    <div class="form-content-ps__right">
+      <?php echo $shortcode; ?>
+    </div>
+  </div>
+</section><!-- form-content-ps end -->
