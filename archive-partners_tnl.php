e47d888a8f8a9ee7a01e6a8a83df3022c15a0e04
tosha504 <tosha.rovniy@gmail.com>
2024-11-06 16:21:09 +0100

    archive-partners_tnl add new features

--- archive-partners_tnl.php
+++ archive-partners_tnl.php
@@ -11,26 +11,32 @@
 get_header();
 ?>
 
-<main id="primary" class="site-main">
-
-	<?php if (have_posts()) {
-		while (have_posts()) {
-			the_post();
-
-			/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-			echo get_the_title();
+<main id="primary" class="site-main partners-tnl">
+	<div class="partners-tnl__banners">
+		<div class="container">
+			<?php echo "<h1>" . post_type_archive_title('', false) . "</h1>"; ?>
+		</div>
+	</div>
+	<div class="container">
+		<?php if (have_posts()) { ?>
+			<ul class="partners-tnl__partners">
+				<?php
+				while (have_posts()) {
+					the_post();
+					echo '<li class="partners-tnl__partners_partner partner">';
+					echo !empty(get_the_post_thumbnail()) ? "<div class='partner__image'>" . get_the_post_thumbnail() . "</div>" : "";
+					echo  !empty(get_the_excerpt()) ? "<div class='partner__excerpt'>" . get_the_excerpt() . "</div>" : "";
+					echo "<a href=" . esc_url(get_the_permalink()) . ">Więcej</a>";
+					echo '</li>';
+				} ?>
+			</ul>
+	</div>
+<?php
+			wp_reset_postdata();
+		} else {
+			get_template_part('template-parts/content', 'none');
 		}
-
-		the_posts_navigation();
-	} else {
-
-		get_template_part('template-parts/content', 'none');
-	}
-	?>
+?>
 
 </main><!-- #main -->
 
