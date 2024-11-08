7b05d6b49b055838daec10803d9ccd9f3a7a0691
tosha504 <tosha.rovniy@gmail.com>
2024-11-08 15:07:30 +0100

    technology-partners-ps block ended

--- blocks/technology-partners-ps/technology-partners-ps.php
+++ blocks/technology-partners-ps/technology-partners-ps.php
@@ -22,21 +22,38 @@ $tag = get_field('tag');
 $text_title = get_field('text_title');
 $class_title = get_field('class_title');
 $font_weight = get_field('font-weight');
-$description = !empty(get_field('description_partners')) ? "<div class='technology-partners-ps__description'>" . get_field('description_partners') . "</div>" : "";
-$partners = get_field('partners'); ?>
+$description = !empty(get_field('description')) ? "<div class='technology-partners-ps__description'>" . get_field('description') . "</div>" : "";
+$partners = get_field('partners');
+$link = get_field('link');  ?>
 <!-- technology-partners-ps start -->
 <section class="technology-partners-ps" <?php echo $anchor; ?>>
   <div class="container">
     <?php show_title_and_btn($tag, $text_title, $class_title, $font_weight);
     echo  $description;
-    if (!empty($partners) && count($partners) > 0) {      ?>
-      <div class="technology-partners-ps__items">
+    if (!empty($partners) && count($partners) > 0) { ?>
+      <ul class="partners-tnl__partners">
         <?php foreach ($partners as $key => $partner) { ?>
-          <div class="technology-partners-ps__items_item item">
-            <?php echo my_custom_attachment_image($partner['partner_logo']);
-            echo !empty($partner['partner_name']) ? "<p>{$partner['partner_name']}</p>" : ""; ?>
-          </div>
+          <li class="partners-tnl__partners_partner partner">
+            <?php if (get_the_post_thumbnail($partner->ID)) { ?>
+              <div class="partner__image"><?php echo get_the_post_thumbnail($partner->ID); ?></div>
+            <?php  }
+            if ($partner->post_excerpt) { ?>
+              <div class="partner__excerpt"><?php echo $partner->post_excerpt; ?></div>
+            <?php  } ?>
+            <a href="<?php echo esc_url(get_the_permalink($partner->ID)); ?>">
+              <?php _e('Read more', 'passus'); ?>
+            </a>
+          </li>
         <?php } ?>
+      </ul>
+    <?php }
+    if ($link) {
+      $link_url = $link['url'];
+      $link_title = $link['title'];
+      $link_target = $link['target'] ? $link['target'] : '_self';
+    ?>
+      <div class="bg-content-breadcrumbs-ps__left_button-wrap">
+        <a class="button button__outline" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
       </div>
     <?php } ?>
   </div>
