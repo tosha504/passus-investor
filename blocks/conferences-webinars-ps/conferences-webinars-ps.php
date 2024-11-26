c959f40fd547a6898101d511eb7622dd17628a90
tosha504 <tosha.rovniy@gmail.com>
2024-11-26 11:13:29 +0100

    Conferences Webinars PS Block  update

--- blocks/conferences-webinars-ps/conferences-webinars-ps.php
+++ blocks/conferences-webinars-ps/conferences-webinars-ps.php
@@ -22,44 +22,39 @@ $tag = get_field('tag');
 $text_title = get_field('text_title');
 $class_title = get_field('class_title');
 $font_weight = get_field('font-weight');
-$events = get_field('events'); ?>
+$events = get_field('events_block'); ?>
 <!-- conferences-webinars-ps start -->
 <section class="conferences-webinars-ps" <?php echo  $anchor; ?>>
   <div class="container">
     <?php show_title_and_btn($tag, $text_title, $class_title, $font_weight);
-    if (!empty($events) && count($events) > 0) {  ?>
+    if (!empty($events) && count($events) > 0) { ?>
       <ul class="conferences-webinars-ps__items">
-        <?php foreach ($events as $key => $event) { ?>
+        <?php foreach ($events as $key => $event) {
+          $curent_id = $event->ID;
+          $date_event = get_field('date_event', $curent_id);
+          $time = get_field('time', $curent_id);
+          $place = get_field('place', $curent_id); ?>
           <li class="conferences-webinars-ps__items_item item">
             <div class="item__image">
-              <?php echo my_custom_attachment_image($event['banner']); ?>
+              <?php echo get_the_post_thumbnail($curent_id); ?>
             </div>
             <div class="item__content">
               <div class="item__content_attr">
-                <p class="date"><?php echo $event['Date']; ?></p>
-                <p class="time"><?php echo $event['time']; ?></p>
+                <p class="date"><?php echo $date_event; ?></p>
+                <p class="time"><?php echo $time; ?></p>
               </div>
               <p class="item__title">
-                <?php echo $event['title']; ?>
+                <?php echo $event->post_title; ?>
               </p>
               <p class="item__place">
-                <?php echo $event['place']; ?>
+                <?php echo $place; ?>
               </p>
-
               <div class="item__description">
-                <?php echo $event['short_descr']; ?>
+                <?php echo $event->post_excerpt; ?>
               </div>
-              <?php
-              $link = $event['button'];
-              if ($link) {
-                $link_url = $link['url'];
-                $link_title = $link['title'];
-                $link_target = $link['target'] ? $link['target'] : '_self';
-              ?>
-                <div class="item__play">
-                  <a class="button button__primary" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>" aria-label="Subscribe to event"><?php echo esc_html($link_title); ?></a>
-                </div>
-              <?php } ?>
+              <div class="item__play">
+                <a class="button button__primary" href="<?php echo esc_url(get_permalink($curent_id)); ?>" aria-label="Subscribe to event"><?php _e('Sign up', 'passus') ?></a>
+              </div>
             </div>
           </li>
         <?php } ?>
