0e4818979e934b3c386b8d297cb924e980ad54f4
tosha504 <tosha.rovniy@gmail.com>
2024-11-26 13:19:08 +0100

    recordings update

--- blocks/recordings-ps/recordings-ps.php
+++ blocks/recordings-ps/recordings-ps.php
@@ -21,44 +21,35 @@ if (!empty($block['anchor'])) {
 $tag = get_field('tag');
 $text_title = get_field('text_title');
 $class_title = get_field('class_title');
-$font_weight = get_field('font-weight'); ?>
+$font_weight = get_field('font-weight');
+$recordings = get_field('recordings'); ?>
 <!-- recordings-ps start -->
 <section class="recordings-ps" <?php echo  $anchor; ?>>
   <div class="container">
-    <?php show_title_and_btn($tag, $text_title, $class_title, $font_weight); ?>
-    <ul class="recordings-ps__items">
-      <li class="recordings-ps__items_item">
-        <div class="recordings-ps__items_item-img">
-          <img src="<?php echo get_template_directory_uri() ?>/assets/images/placeholders/placeholder-video-image.png" alt="">
-        </div>
-        <div class="item-wrap">
-          <p class="item-wrap__category">Cyberbezpieczeństwo</p>
-          <p class="item-wrap__title">Reaktywne vs proaktywne podejście do cyberbezpieczeństwa</p>
-          <a href="#" class="button button__primary"><?php _e('Button', 'passus'); ?></a>
-        </div>
-      </li>
+    <?php show_title_and_btn($tag, $text_title, $class_title, $font_weight);
+    if (!empty($recordings) && count($recordings) > 0) { ?>
+      <ul class="recordings-ps__items">
+        <?php foreach ($recordings as $key => $record) {
+          $curent_id = $record->ID;
+          $categories = wp_get_post_terms($curent_id, 'categories_webinar');
+          // var_dump($categories[0]->name);
+        ?>
+          <li class="recordings-ps__items_item">
+            <div class="recordings-ps__items_item-img">
+              <?php echo  my_custom_attachment_image(get_post_thumbnail_id($curent_id)); ?>
+            </div>
+            <div class="item-wrap">
+              <p class="item-wrap__category">
+                <?php if (!empty($categories[0])) {
+                  echo $categories[0]->name;
+                }  ?></p>
 
-      <li class="recordings-ps__items_item">
-        <div class="recordings-ps__items_item-img">
-          <img src="<?php echo get_template_directory_uri() ?>/assets/images/placeholders/placeholder-video-image.png" alt="">
-        </div>
-        <div class="item-wrap">
-          <p class="item-wrap__category">Ciągłość działania IT</p>
-          <p class="item-wrap__title">Reaktywne vs proaktywne podejście do cyberbezpieczeństwa</p>
-          <a href="#" class="button button__primary"><?php _e('Button', 'passus'); ?></a>
-        </div>
-      </li>
-
-      <li class="recordings-ps__items_item">
-        <div class="recordings-ps__items_item-img">
-          <img src="<?php echo get_template_directory_uri() ?>/assets/images/placeholders/placeholder-video-image.png" alt="">
-        </div>
-        <div class="item-wrap">
-          <p class="item-wrap__category">Ciągłość działania IT</p>
-          <p class="item-wrap__title">Reaktywne vs proaktywne podejście do cyberbezpieczeństwa </p>
-          <a href="#" class="button button__primary"><?php _e('Button', 'passus'); ?></a>
-        </div>
-      </li>
-    </ul>
+              <p class="item-wrap__title">Reaktywne vs proaktywne podejście do cyberbezpieczeństwa</p>
+              <a href="#" class="button button__primary"><?php _e('Button', 'passus'); ?></a>
+            </div>
+          </li>
+        <?php } ?>
+      </ul>
+    <?php } ?>
   </div>
 </section><!-- recordings-ps end -->
